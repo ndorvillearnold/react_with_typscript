891e794ed88360ffb295c41fc1d8488adb97b58a
Naomy <ndorvillearnold@gmail.com>
2024-04-04 13:15:48 -0400

    added type script interface t catch errors

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import axios from "axios";
 
-const url = "https://jsonplaceholder.typicode.com/todos";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
 axios.get(url).then((response) => {
-	console.log(response.data);
+	const todo = response.data as Todo;
+	//reponse .data has properties of id, titile, completed
+	//inerface defines a structure of an object
+
+	const id = todo.id;
+	const title = todo.title;
+	const completed = todo.completed;
+
+	console.log(`
+	The Todo with ID: ${id} 
+	Has a title of : ${title}
+	Is it completed> ${completed}
+	
+	`);
 });
